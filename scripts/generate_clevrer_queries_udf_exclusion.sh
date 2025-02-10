e69ad4e36fc3c76073c82e5a8dbbcfd7b3f69898
Enhao Zhang <enhaoz@cs.washington.edu>
2025-02-09 18:15:58 -0800

    cleanup

--- scripts/generate_clevrer_queries_udf_exclusion.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-declare -a UDFArray=("Near" "Far" "Behind" "RightOf" "Right" "Bottom" "Color_brown" "Color_cyan" "Color_purple" "Color_yellow" "Shape_cylinder" "Material_metal")
-
-for udf in "${UDFArray[@]}"; do
-    sbatch generate_clevrer_queries_udf_exclusion.sbatch $udf
-done
--- scripts/test_distill_model.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for n_train_distill in 500; do
-    for udf_idx in {0..9}; do
-        sbatch test_distill_model.sbatch $udf_idx $n_train_distill "charades"
-    done
-done
